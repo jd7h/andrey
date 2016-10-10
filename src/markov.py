04ca320f4236a6781e69661447eec2fd1c0fda70
Judith van Stegeren <git@jd7h.com>
2016-10-10 10:05:16 +0200

    Added generator

--- src/markov.py
+++ src/markov.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+import random
 from collections import defaultdict
 
 def open_source_text(filename):
@@ -26,4 +27,27 @@ def build_markov_chain(source, order=1):
         markov[t].append(sourcelist[i+order])
     return markov
 
+def generate_text(markovchain,length):
+    text = []
+
+    # choose first sequence
+    current_state = random.choice(list(markovchain.keys()))
+
+    while len(text) < length:
+        print("Current state:",current_state)
+        next_word = random.choice(markovchain[current_state])
+        print("next_word:",next_word)
+        current_state = current_state[1:] + tuple([next_word])
+        text.append(next_word)
+
+    return text
+
+def main():
+    source = open_source_text("../texts/pride-and-prejudice-full-text")
+    mkv = build_markov_chain(source,5)
+    text = generate_text(mkv,1000)
+    return text
+
+if __name__ == "__main__":
+    main()
 
