6f5a44a1e1d4bb1084916662912da1e35992944f
Judith van Stegeren <git@jd7h.com>
2017-10-01 19:23:20 +0200

    Add docstrings, better naming, example usage

--- src/markov.py
+++ src/markov.py
@@ -8,6 +8,7 @@ def open_source_text(filename):
     return source_text
 
 def clean_source(source):
+    """Clean source text by fixing capitalization and special chars."""
     source = source.replace("_","")
     source = source.replace("."," . ")
     source = source.replace(","," , ")
@@ -18,24 +19,35 @@ def clean_source(source):
     wordset = set(source.split())
     for word in wordset:
         if not word.lower() in wordset:
-            pass
+            pass # for names
         else:
             source = source.replace(word,word.lower())
     return source
 
-def build_markov_chain(source, order=1):
+def build_markov_chain(source, n=1):
+    """Build a nth-n markov chain based on a source text.
+    
+    Keyword arguments:
+    source -- cleaned source text
+    n -- the desired order of the markov chain (default 1)
+    """
     markov = {}
-    source = clean_source(source)
     sourcelist = source.split()
     
-    for i in range(0,len(sourcelist)-order):
-        t = tuple(sourcelist[i:i+order])
+    for i in range(0,len(sourcelist)-n):
+        t = tuple(sourcelist[i:i+n])
         if not t in markov.keys():
             markov[t] = []
-        markov[t].append(sourcelist[i+order])
+        markov[t].append(sourcelist[i+n])
     return markov
 
-def generate_text(markovchain,length):
+def generate_text(markovchain,length=50):
+    """Generate a new text based on the given Markov chain.
+    
+    Keyword arguments:
+    markovchain -- the markov chain
+    length -- desired length in words of the text (default 50)
+    """
     text = []
 
     # choose first sequence
@@ -50,11 +62,10 @@ def generate_text(markovchain,length):
 
 def main():
     source = open_source_text("../texts/pride-and-prejudice")
-    source2 = open_source_text("../texts/emma")
-    source3 = open_source_text("../texts/sense-and-sensibility")
-    source = source + source2 + source3
-    mkv = build_markov_chain(source,4)
+    source = clean_source(source)
+    mkv = build_markov_chain(source,2)
     text = generate_text(mkv,300)
+    print(" ".join(text))
     return text
 
 if __name__ == "__main__":
